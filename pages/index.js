89e8aef89cd7862ce190d938ee957a8443b57a32
Kelly Tarnowski <41117529+kllytarnowski@users.noreply.github.com>
2022-04-29 10:57:22 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Kelly Tarnowski Portfolio!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Content will be added. <code>pages/index.js</code>
         </p>
       </main>
 
